f4f536806d71c8f8e1b80051164dea8c537056c6
Patate <patate@joepegs.com>
2024-02-14 14:30:30 +0100

    add deploy contract script

---
+++ script/DeployContracts.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {console2 as console} from "forge-std/src/console2.sol";
+
+import {Script} from "forge-std/src/Script.sol";
+import {Shoe404, IDescriptor} from "src/Shoe404.sol";
+import {Shoe404Mirror} from "src/Shoe404Mirror.sol";
+import {ShoeDescriptor} from "src/ShoeDescriptor.sol";
+
+contract ShoeContractDeployment is Script {
+    address constant ROYALTY_RECEIVER = 0x3DF701bFf974aFaE3F5A213fd52D3400f9D764c9;
+    uint96 constant ROYALTY_BPS = 50; // 0,5%
+
+    function run() public returns (Shoe404 shoe, Shoe404Mirror mirror, ShoeDescriptor descriptor) {
+        address deployer = vm.rememberKey(vm.envUint("DEPLOYER_PRIVATE_KEY"));
+        address airdropManager = vm.rememberKey(vm.envUint("AIRDROP_PRIVATE_KEY"));
+
+        vm.createSelectFork("http://127.0.0.1:8545");
+
+        vm.startBroadcast(deployer);
+        shoe = new Shoe404("Shoe404", "SHOE", 10_000e18, airdropManager);
+        descriptor = new ShoeDescriptor(airdropManager);
+        mirror = Shoe404Mirror(payable(shoe.mirrorERC721()));
+        vm.stopBroadcast();
+
+        vm.startBroadcast(airdropManager);
+        mirror.pullOwner();
+        descriptor.setBaseURI("ipfs://bafybeihzq3eaeurh4tqxajfy7lfvvajh57ahlrfliuf54bhaf555pnbc5m/");
+        shoe.setDescriptor(IDescriptor(address(descriptor)));
+        mirror.setDefaultRoyalty(ROYALTY_RECEIVER, ROYALTY_BPS);
+    }
+}
